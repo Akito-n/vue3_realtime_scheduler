cc009eb7bbd407e9bf2b0206f7e29f236e6074d1
Akito Nagai <a.nagai@mof-mof.co.jp>
2020-06-01 14:39:36 +0900

    ディレクトリとカレンダー雛形作る

---
+++ app/javascript/vue/pages/TheCalender.vue
@@ -0,0 +1,10 @@
+<template> </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { defineComponent } from '@vue/composition-api'
+import { useCurrentUserQuery } from '@/graphql/types'
+
+export default {}
+</script>
+<style scoped></style>
