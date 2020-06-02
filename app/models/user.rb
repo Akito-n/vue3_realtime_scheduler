9c713b94b6baafc2f07273978f100b443652cef0
Akito Nagai <a.nagai@mof-mof.co.jp>
2020-06-02 20:45:20 +0900

    Add annotate

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,11 +12,9 @@
 #  encrypted_password     :string           default(""), not null
 #  last_sign_in_at        :datetime
 #  last_sign_in_ip        :inet
-#  name                   :string
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
-#  role                   :integer          not null
 #  sign_in_count          :integer          default(0), not null
 #  unconfirmed_email      :string
 #  created_at             :datetime         not null
