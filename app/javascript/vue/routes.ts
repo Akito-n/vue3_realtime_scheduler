9700c1d19ebf47dc324e95faa7d3aad6cabb4289
Akito Nagai <a.nagai@mof-mof.co.jp>
2020-06-08 23:29:58 +0900

    formatに変更するのとcalender用のpathを整える

--- app/javascript/vue/routes.ts
+++ app/javascript/vue/routes.ts
@@ -4,7 +4,13 @@ import TheWeekCalendar from './pages/TheWeekCalendar.vue'
 import Protected from './Protected.vue'
 import Admin from './Admin.vue'
 import AdminDashBoard from './pages/AdminDashBoard.vue'
-import { getYear, getMonth, getDate } from 'date-fns'
+import { format, addMonths, parse } from 'date-fns'
+
+//funcsに分ける？
+const today = new Date()
+const year = format(today, 'yyyy')
+const month = format(addMonths(today, 1), 'MM')
+const day = format(today, 'dd')
 
 export const routes = [
   {
@@ -15,11 +21,13 @@ export const routes = [
       { path: 'calendar/week/:year/:month/:day', component: TheWeekCalendar },
       {
         path: 'calendar/week',
-        redirect: `/calendar/week/${getYear(new Date())}/${
-          getMonth(new Date()) + 1
-        }/${getDate(new Date())}`
+        redirect: `/calendar/week/${year}/${month}/${day}`
       },
-      { path: 'calendar', component: TheCalendar }
+      { path: 'calender/month/:year/:month', component: TheCalendar },
+      {
+        path: 'calendar',
+        redirect: `/calender/month/${year}/${month}`
+      }
     ]
   },
   {
