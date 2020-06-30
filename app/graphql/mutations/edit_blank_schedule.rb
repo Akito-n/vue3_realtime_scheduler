f5fc26d46463a95675737fea9ee7b2c9ebcf3165
shwld <shu.account@outlook.com>
2020-06-30 12:34:04 +0900

    Implement editBlankSchedule

---
+++ app/graphql/mutations/edit_blank_schedule.rb
@@ -0,0 +1,30 @@
+class Mutations::EditBlankSchedule < Mutations::BaseMutation
+  null true
+
+  argument :blank_schedule_id, ID, required: true, loads: Types::Objects::BlankScheduleType
+  argument :start_at, Types::Scalars::DateTime, required: false
+  argument :end_at, Types::Scalars::DateTime, required: false
+
+  field :blank_schedule, Types::Objects::BlankScheduleType, null: true
+
+  def authorized?(blank_schedule:, **args)
+    context[:user_signed_in] && blank_schedule.can_write?(context[:current_user])
+  end
+
+  def resolve(blank_schedule:, **input)
+    if blank_schedule.update(input.compact)
+      AppSchema.subscriptions.trigger('schedules', {}, {})
+      {
+        blank_schedule: blank_schedule
+      }
+    else
+      set_errors(blank_schedule)
+      return
+    end
+  end
+
+  def set_errors(blank_schedule)
+    message = blank_schedule.errors.full_messages.join(', ')
+    context.add_error(GraphQL::ExecutionError.new(message))
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,7 @@
 module Types
   class MutationType < Types::BaseObject
     field :add_blank_schedule, mutation: Mutations::AddBlankSchedule
+    field :edit_blank_schedule, mutation: Mutations::EditBlankSchedule
     field :edit_user, mutation: Mutations::EditUser
     field :request_schedule_to_occupation, mutation: Mutations::RequestScheduleToOccupation
     field :request_schedule_to_individual_user, mutation: Mutations::RequestScheduleToIndividualUser
