4bd9dad3590423b28ed93b0338cc672e26e1b75e
Akito Nagai <a.nagai@mof-mof.co.jp>
2020-07-03 15:08:07 +0900

    色を似たものに変更

--- db/fixtures/02occupation.rb
+++ db/fixtures/02occupation.rb
@@ -5,15 +5,15 @@ sawada = User.find_by(email: 'sawadashoten@pca.com')
 Occupation.seed(
   #戸澤総研
   {user: tozawa, name: 'ITコンサルタント', color: 'red'},
-  {user: tozawa, name: 'PM', color: 'hotpink'},
-  {user: tozawa, name: 'sales manager', color: 'salmon'},
+  {user: tozawa, name: 'PM', color: 'pink'},
+  {user: tozawa, name: 'sales manager', color: 'purple'},
   #KANEKOトイズファクトリー
   {user: kaneko, name: 'インフラエンジニア', color: 'orange'},
-  {user: kaneko, name: '生産管理', color: 'khaki'},
-  {user: kaneko, name: '営業アシスタント', color: 'lemonchiffon'},
+  {user: kaneko, name: '生産管理', color: 'gray'},
+  {user: kaneko, name: '営業アシスタント', color: 'yellow'},
   #澤田商店
-  {user: sawada, name: 'インサイドセールス', color: 'mediumaquamarine'},
-  {user: sawada, name: '経理', color: 'aquamarine'},
-  {user: sawada, name: '社内SE', color: 'lime'},
+  {user: sawada, name: 'インサイドセールス', color: 'green'},
+  {user: sawada, name: '経理', color: 'teal'},
+  {user: sawada, name: '社内SE', color: 'indigo'},
 
 )
