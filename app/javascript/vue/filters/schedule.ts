8a689be312679f8350aecef7fe49645f6aafa2a8
Akito Nagai <a.nagai@mof-mof.co.jp>
2020-08-03 12:44:09 +0900

    表示ロジック

--- app/javascript/vue/filters/schedule.ts
+++ app/javascript/vue/filters/schedule.ts
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Schedule } from 'graphql/types'
+import { Schedule, User, Occupation } from 'graphql/types'
 import { differenceInMinutes } from 'date-fns'
 
 export const scheduleFilter = () => {
@@ -11,10 +11,28 @@ export const scheduleFilter = () => {
       return value.responder.name
     }
   })
-  Vue.filter('sample', (value: Schedule) => {
-    if (!value) return ''
-    return (
-      differenceInMinutes(new Date(value.endAt), new Date(value.startAt)) / 30
-    )
+  Vue.filter('calenderInformation', (value: Schedule, user: User) => {
+    if (!value && !user) return ''
+    if (user.isIndividual) {
+      if (value.isRequest && value.status == '確定済み') {
+        return '【面接】' + value.occupation.companyName
+      } else if (value.isRequest && value.mine) {
+        return '【リクエスト送信】' + value.occupation.companyName
+      } else if (value.isRequest && !value.mine) {
+        return '【リクエスト受信】' + value.occupation.companyName
+      } else {
+        return '【面接可能】'
+      }
+    } else {
+      if (value.isRequest && value.status == '確定済み') {
+        return '【面接】' + value.responder.companyName
+      } else if (value.isRequest && value.mine) {
+        return '【リクエスト送信】' + value.responder.companyName
+      } else if (value.isRequest && !value.mine) {
+        return '【リクエスト受信】' + value.responder.companyName
+      } else {
+        return '【面接可能】'
+      }
+    }
   })
 }
